4706623cce339aa1034812613f15936859bab9f8
Uzair Abdullah <uzairabdullah973@gmail.com>
2021-02-11 16:10:14 +0500

    hyuhg

--- my-app/src/ComponentsOfPage/ComponentsOfHome/WeatherBoxes.js
+++ my-app/src/ComponentsOfPage/ComponentsOfHome/WeatherBoxes.js
@@ -4,8 +4,13 @@ function WeatherBoxes(){
     return(
         <section id="w-b-s">
             <div id="w-b-d">
-                <div></div>
-                <div></div>
+                <div style={{width:"100%",height:"100%",backgroundColor:"gray",margin:"0"}} className="row">
+                    <div style={{width:"100%",height:"auto",backgroundColor:"blue"}} className="w-c col-lg-6 col-md-12 col-sm-12">
+                        <div className="w-boxes"></div>
+                        <div className="w-boxes"></div>
+                    </div>
+                    <div style={{width:"100%",height:"auto",backgroundColor:"red"}} className="w-c col-lg-6 col-md-12 col-sm-12"></div>
+                </div>
             </div>
         </section>
     )
